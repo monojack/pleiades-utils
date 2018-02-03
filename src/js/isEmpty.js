54fcd84db9806e750b9947193fc9511205ef75f0
Ionut Achim <iachim@axway.com>
2018-02-03 18:57:37 +0200

    Fix `isEmpty` export

--- src/js/isEmpty.js
+++ src/js/isEmpty.js
@@ -1,6 +1,6 @@
-import isType from './isType'
+import { isType, } from './isType'
 
-export default function isEmpty (value) {
+export function isEmpty (value) {
   if (typeof value === 'string') return !value
   if (isType('object', value)) return !Object.values(value).length
   if (isType('array', value)) return !value.length
